2cc211e903ca198768a8ea4c57ddb967de66d2bb
James <james.peng.pkj@gmail.com>
2025-12-25 10:24:10 +0800

    make it better

--- scripts/iltv_m3ugrabber.py
+++ scripts/iltv_m3ugrabber.py
@@ -2,7 +2,7 @@ import requests
 import os
 import sys
 
-HEADERS={"User-Agent": "okhttp/4.12.0"}
+HEADERS={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"}
 
 windows = False
 if 'win' in sys.platform:
@@ -13,26 +13,28 @@ def grab(url):
     end = 1000
     response = requests.get(url, headers=HEADERS, timeout=15).text
     if '&token=' in response:
+        print('requests get token ...')
         start = response.find('&token=') + 7
         end = start + 64
     else:
-        os.system(f'curl --header "User-Agent: okhttp" "{url}" > temp.txt')
+        os.system(f'curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36" "{url}" > temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '&token=' in response:
+            print('curl get token ...')
             start = response.find('&token=') + 7
             end = start + 64
     return f"{response[start : end]}"
 
 token=grab('https://tv.iill.top/m3u/Gather')
 print(token)
-with open('iltv_template.m3u','r',encoding='utf-8') as f:
+with open('scripts/iltv_template.m3u','r',encoding='utf-8') as f:
     txt = f.read().replace('&token=key','&token='+str(token))
-    with open('../m3u/iltv.m3u','w',encoding='utf-8') as g:
+    with open('m3u/iltv.m3u','w',encoding='utf-8') as g:
         g.write(txt)
 
-with open('iltv_template.txt','r',encoding='utf-8') as f:
+with open('scripts/iltv_template.txt','r',encoding='utf-8') as f:
     txt = f.read().replace('&token=key','&token='+str(token))
-    with open('../m3u/iltv.txt','w',encoding='utf-8') as g:
+    with open('m3u/iltv.txt','w',encoding='utf-8') as g:
         g.write(txt)
 
 if 'temp.txt' in os.listdir():
